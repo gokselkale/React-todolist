061fe2909c59da19591fcc14969f6af1e7005712
Göksel Kale <gokselkalekale@gmail.com>
2022-10-19 23:10:33 +0300

    Dosyalar Yüklendi

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Components from './Components';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Components />
+    </div>
+  );
+}
+
+export default App;
